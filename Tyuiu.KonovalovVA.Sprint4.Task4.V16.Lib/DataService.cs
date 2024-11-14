b60c20151cdb87a85b4f514c3dea2f2a05e2a868
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-11-14 11:29:08 +0500

    Task4

---
+++ Tyuiu.KonovalovVA.Sprint4.Task4.V16.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.KonovalovVA.Sprint4.Task4.V16.Lib
+{
+    public class DataService : ISprint4Task4V16
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 == 2) matrix[i, j] = 1;
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.KonovalovVA.Sprint4.Task4.V16/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.KonovalovVA.Sprint4.Task4.V16.Lib;
+namespace Tyuiu.KonovalovVA.Sprint4.Task4.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int[,] ar = new int[5,5];
+            for (int i = 0; i < ar.GetLength(0); i++)
+            {
+                for (int j = 0; j < ar.GetLength(1); j++)
+                {
+                    Console.WriteLine("Введите" +i+" "+j +" элемент массива");
+                    ar[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.Title = "Спринт #4 | Выполнил: Коновалов В.А. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Коновалов Владимир Артёмович | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < ar.GetLength(0); i++)
+            {
+                for (int j = 0; j < ar.GetLength(1); j++)
+                    Console.Write(ar[i, j] + " ");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(ar));
+        }
+    }
+}
