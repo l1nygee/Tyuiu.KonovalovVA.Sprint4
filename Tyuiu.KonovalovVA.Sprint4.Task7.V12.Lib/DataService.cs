da7a5f7bf2d0d53a7e490d3b8bba17fe8eeeedd8
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-11-14 12:10:36 +0500

    Task7

---
+++ Tyuiu.KonovalovVA.Sprint4.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.KonovalovVA.Sprint4.Task7.V12.Lib
+{
+    public class DataService : ISprint4Task7V12
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] ar = new int[n, m];
+            int res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    ar[i,j] = int.Parse(value[i*n+j].ToString());
+                    if (ar[i, j] % 2 != 0) res += ar[i, j];
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KonovalovVA.Sprint4.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.KonovalovVA.Sprint4.Task7.V12.Lib;
+namespace Tyuiu.KonovalovVA.Sprint4.Task7.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int n = 3;
+            int m = 4;
+            string st = "658932125478";
+            int res = ds.Calculate(n, m, st);
+            int wait = 33;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
